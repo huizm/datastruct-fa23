e5255170faeb39234bcde181190d8c3b0a1faf05
Zeming Hui <me@huizm.cn>
2023-10-16 13:05:45 +0800

    feat(hw): hw2-4

--- hw/restricted-linear-list/hw2-4.c
+++ hw/restricted-linear-list/hw2-4.c
@@ -11,3 +11,64 @@
 // 用例4：输入：abced
 // 输出：匹配未成功
 // -10000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define txt "abacababdeadcde"
+
+int *Next(const char * restrict pat);
+bool KMPNext(const char * restrict pat, const int * restrict next);
+
+int main(void) {
+    char pat[16];
+    scanf("%s", pat);
+    int *n = Next(pat);
+    bool doesMatch = KMPNext(pat, n);
+
+    if (doesMatch)
+        printf("匹配成功\n");
+    else
+        printf("匹配未成功\n");
+
+    size_t n_len = strlen(pat);
+    for (unsigned i = 0; i < n_len; i++)
+        printf("%d", n[i]);
+    return 0;
+}
+
+int *Next(const char * restrict pat) {
+    size_t len = strlen(pat);
+    int *n = (int *)malloc(len * sizeof(int));
+    if (n == NULL)
+        exit(-1);
+
+    n[0] = -1;
+    n[1] = 0;
+    for (unsigned i = 1U; i < len - 1; i++) {
+        if (pat[i] == pat[n[i]])
+            n[i + 1] = n[i] + 1;
+        else
+            n[i + 1] = 0;
+    }
+
+    return n;
+}
+
+bool KMPNext(const char * restrict pat, const int * restrict next) {
+    size_t n = strlen(txt);
+    size_t m = strlen(pat);
+    int i, j;
+
+    for (i = 0, j = 0; i < n && j < m; i++, j++) {
+        if (pat[j] != txt[i])
+            j = next[j];
+    }
+
+    if (j == m)
+        return true;
+    else
+        return false;
+}
