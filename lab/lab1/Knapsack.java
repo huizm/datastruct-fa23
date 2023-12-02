2a9e36c9ea832045f5fbdd00d1510b3b8193e313
Zeming Hui <me@huizm.cn>
2023-12-03 05:21:50 +0800

    feat(lab1): implement knapsack problem

---
+++ lab/lab1/Knapsack.java
@@ -0,0 +1,17 @@
+public class Knapsack {
+
+    private final long cap;
+
+    public Knapsack(long cap) {
+        this.cap = cap;
+    }
+
+    public long[][] solve(long[] volume) {
+        return null;
+    }
+
+    public long[][] solve(long[] volume, long[] value) {
+        return null;
+    }
+
+}
